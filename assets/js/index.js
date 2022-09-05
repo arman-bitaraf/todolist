ea794d3f122e6d68edf9f6106ac8a755091e4cd0
arman-bitaraf <armanbitaraf@gmail.com>
2022-09-05 13:00:33 +0200

    feat: js codes
    - craete js codes of btn
    - create list for inputs

---
+++ assets/js/index.js
@@ -0,0 +1,15 @@
+let addBtn = document.querySelector('button');
+let taskList =document.querySelector('ul');
+let input = document.querySelector('input');
+
+function createTask(text){
+    let li = document.createElement('li');
+    li.textContent= text;
+    return li
+}
+addBtn.addEventListener('click', ()=>{
+    let text = input.value;
+    let task = createTask(text);
+    taskList.appendChild(task);
+    input.value='';
+})
